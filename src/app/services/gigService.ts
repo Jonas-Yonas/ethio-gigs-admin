b4f8ff383d810e278d3c934e45fced9b7f9821ac
@yonas_gashaw <Yonas1520@gmail.com>
2025-04-21 15:10:59 +0300

    Route issue fix

--- src/app/services/gigService.ts
+++ src/app/services/gigService.ts
@@ -3,7 +3,7 @@ import { Gig } from "@/types/gig";
 const fetchGigs = async (currentPage: number) => {
   try {
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/gigs?page=${currentPage}`
+      `${process.env.NEXT_PUBLIC_API_URL}/admin/gigs?page=${currentPage}`
     );
 
     const data = await res.json();
